0df3d15e5dfb932341ccb5c2ed593f9f525ae621
EslamFares <eslamfareselazb@gmail.com>
2020-04-27 17:41:15 +0200

    first

---
+++ lib/HomePage.dart
@@ -0,0 +1,413 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _scafoldKey = GlobalKey<ScaffoldState>();
+  AssetImage cross = AssetImage('assets/img/cross.png');
+  AssetImage circle = AssetImage('assets/img/circle00.png');
+  AssetImage edit = AssetImage('assets/img/empty.png');
+
+  int crossWin = 0;
+  int circleWin = 0;
+  int draw = 0;
+  List<String> gameState;
+  bool isCross = true;
+  bool gameEnd = false;
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      gameState = [
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+      ];
+    });
+  }
+
+  AssetImage getImage(String value) {
+    switch (value) {
+      case ('empty'):
+        return edit;
+        break;
+      case ('cross'):
+        return cross;
+        break;
+      case ('circle'):
+        return circle;
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scafoldKey,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent, //Color(0xffFF0000)
+        centerTitle: true,
+        title: Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: Column(
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'X',
+                    style: TextStyle(
+                        color: Color(0xffFF0000),
+                        fontSize: 30,
+                        letterSpacing: 1.2,
+                        fontFamily: GoogleFonts.bangers().fontFamily),
+                  ),
+                  Text(
+                    'O ',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 30,
+                        letterSpacing: 1.2,
+                        fontFamily: GoogleFonts.bangers().fontFamily),
+                  ),
+                  Text(
+                    'Game',
+                    style: TextStyle(
+                        color: Colors.amber,
+                        fontSize: 30,
+                        letterSpacing: 1.2,
+                        fontFamily: GoogleFonts.bangers().fontFamily),
+                  ),
+                ],
+              ),
+              Container(
+                height: 10,
+                width: 250,
+                color: Colors.red,
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 15),
+            height: MediaQuery.of(context).size.height/1.65,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              alignment: Alignment.center,
+         children: <Widget>[
+           Image.asset('assets/img/backg3.png',
+             fit: BoxFit.cover,
+             width: MediaQuery.of(context).size.width,
+             height: MediaQuery.of(context).size.height/1.5,),
+             GridView.builder(
+               padding: EdgeInsets.all(20),
+               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                   crossAxisCount: 3,
+                   childAspectRatio: 1.0,
+                   crossAxisSpacing: 5.0,
+                   mainAxisSpacing: 5.0),
+               itemBuilder: (context, i) => SizedBox(
+                 height: 100,
+                 width: 100,
+                 child: MaterialButton(
+                   onPressed: () => playGame(i),
+                   child: Image(
+                     image: getImage(gameState[i]),
+                   ),
+                 ),
+               ),
+               itemCount: gameState.length,
+             ),
+         ],
+            ),
+
+          ),
+          SizedBox(height: 20,),
+          SizedBox(
+            height: 110,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'X',
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.red,
+                          fontFamily: GoogleFonts.indieFlower().fontFamily,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 0,
+                      ),
+                      Text('${crossWin}',
+                          style: TextStyle(
+                            fontSize: 32,
+                            color: Colors.amber,
+                            fontFamily: GoogleFonts.bangers().fontFamily,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'O',
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.blue,
+                          fontFamily: GoogleFonts.indieFlower().fontFamily,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 0,
+                      ),
+                      Text('${circleWin}',
+                          style: TextStyle(
+                            fontSize: 32,
+                            color: Colors.amber,
+                            fontFamily: GoogleFonts.bangers().fontFamily,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'Draw',
+                        style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.teal,
+                          fontFamily: GoogleFonts.indieFlower().fontFamily,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 0,
+                      ),
+                      Text('${draw}',
+                          style: TextStyle(
+                            fontSize: 32,
+                            color: Colors.amber,
+                            fontFamily: GoogleFonts.bangers().fontFamily,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 5,),
+          Padding(
+            padding: EdgeInsets.only(left: 25,right: 25,top: 20,bottom: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                MaterialButton(
+                  color: Colors.teal.withOpacity(.4),
+                  minWidth: 50,
+                  height: 50,
+                  shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Text('New Game',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      )),
+                  onPressed: () => newGame(),
+                ),
+                MaterialButton(
+                  color: Colors.blueGrey.withOpacity(.5),
+                  minWidth: 50,
+                  height: 50,
+                  shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0)),
+                  child: Text('Play Again',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white70,
+                      )),
+                  onPressed: () => resetGame(),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  playGame(int index) {
+    if (gameState[index] == 'empty') {
+      setState(() {
+        if (isCross) {
+          gameState[index] = 'cross';
+        } else {
+          gameState[index] = 'circle';
+        }
+        isCross = !isCross;
+        checkWin();
+      });
+    }
+  }
+
+  checkWin() {
+    if (!gameEnd) {
+      if ((gameState[0]) != 'empty' &&
+          (gameState[0] == gameState[1]) &&
+          (gameState[1] == gameState[2])) {
+        setState(() {
+          showWiner(gameState[0]);
+          markPoints(gameState[0]);
+          gameEnd = true;
+        });
+      } else if ((gameState[3]) != 'empty' &&
+          (gameState[3] == gameState[4]) &&
+          (gameState[4] == gameState[5])) {
+        setState(() {
+          showWiner(gameState[3]);
+          markPoints(gameState[3]);
+          gameEnd = true;
+        });
+      } else if ((gameState[6]) != 'empty' &&
+          (gameState[6] == gameState[7]) &&
+          (gameState[7] == gameState[8])) {
+        setState(() {
+          showWiner(gameState[6]);
+          markPoints(gameState[6]);
+          gameEnd = true;
+        });
+      } else if ((gameState[0]) != 'empty' &&
+          (gameState[0] == gameState[3]) &&
+          (gameState[3] == gameState[6])) {
+        setState(() {
+          showWiner(gameState[0]);
+          markPoints(gameState[0]);
+          gameEnd = true;
+        });
+      } else if ((gameState[1]) != 'empty' &&
+          (gameState[1] == gameState[4]) &&
+          (gameState[4] == gameState[7])) {
+        setState(() {
+          showWiner(gameState[1]);
+          markPoints(gameState[1]);
+          gameEnd = true;
+        });
+      } else if ((gameState[2]) != 'empty' &&
+          (gameState[2] == gameState[5]) &&
+          (gameState[5] == gameState[8])) {
+        setState(() {
+          showWiner(gameState[2]);
+          markPoints(gameState[2]);
+          gameEnd = true;
+        });
+      } else if ((gameState[0]) != 'empty' &&
+          (gameState[0] == gameState[4]) &&
+          (gameState[4] == gameState[8])) {
+        setState(() {
+          showWiner(gameState[0]);
+          markPoints(gameState[0]);
+          gameEnd = true;
+        });
+      } else if ((gameState[2]) != 'empty' &&
+          (gameState[2] == gameState[4]) &&
+          (gameState[4] == gameState[6])) {
+        setState(() {
+          showWiner(gameState[2]);
+          markPoints(gameState[2]);
+          gameEnd = true;
+        });
+      } else if (!gameState.contains('empty')) {
+        setState(() {
+          showWiner('Game Draw');
+          markPoints('draw');
+          gameEnd = true;
+        });
+      }
+    }
+  }
+
+  resetGame() {
+    setState(() {
+      gameState = [
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+        'empty',
+      ];
+      gameEnd = false;
+    });
+  }
+
+  newGame() {
+    resetGame();
+    setState(() {
+      circleWin = 0;
+      crossWin = 0;
+      draw = 0;
+    });
+  }
+
+  markPoints(String mark) {
+    switch (mark) {
+      case ('cross'):
+        setState(() {
+          crossWin += 1;
+        });
+        break;
+      case ('circle'):
+        setState(() {
+          circleWin += 1;
+        });
+        break;
+      case ('draw'):
+        setState(() {
+          draw += 1;
+        });
+        break;
+    }
+  }
+
+  showWiner(String winer) {
+    _scafoldKey.currentState.showSnackBar(SnackBar(
+      content: Text('${winer} wins'),
+      duration: Duration(seconds: 2),
+    ));
+
+    //to restGame by self with out click in button
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        resetGame();
+      });
+    });
+  }
+}
